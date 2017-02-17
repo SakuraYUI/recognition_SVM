710040171ad62f0d39d6da3d8f0f50de77da02f4
SakuraYUI <wind_flurry@126.com>
2017-02-17 10:59:09 +0800

    recongnition

---
+++ initdatabase.py
@@ -0,0 +1,7 @@
+import codecs 
+
+start, end = (0x4E00, 0x9FA5) 
+
+with codecs.open("database.txt", "wb", encoding="utf-8") as f: 
+  for codepoint in range(int(start), int(end)): 
+    f.write(unichr(codepoint)) 
